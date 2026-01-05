ef9945cc7676f7775e45c9b293bdcce535e4b16f
Gary Grumbley <ggrumbley@classy.org>
2026-01-05 09:50:40 -0800

    feat: wire up database

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+import env from "./lib/env";
+
+const authToken = env.NODE_ENV === "development"
+  ? undefined
+  : env.TURSO_AUTH_TOKEN;
+
+export default defineConfig({
+  out: "./lib/db/migrations",
+  schema: "./lib/db/schema/index.ts",
+  dialect: "turso",
+  casing: "snake_case",
+  dbCredentials: {
+    url: env.TURSO_DATABASE_URL,
+    authToken,
+  },
+});
---
+++ lib/db/index.ts
@@ -0,0 +1,20 @@
+import { drizzle } from "drizzle-orm/libsql";
+
+import env from "~/lib/env";
+
+import * as schema from "./schema";
+
+const authToken = env.NODE_ENV === "development"
+  ? undefined
+  : env.TURSO_AUTH_TOKEN;
+
+const db = drizzle({
+  connection: {
+    url: env.TURSO_DATABASE_URL,
+    authToken,
+  },
+  casing: "snake_case",
+  schema,
+});
+
+export default db;
