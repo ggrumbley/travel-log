8da10826128bd5ab3e144882b2322e6022556482
Gary Grumbley <ggrumbley@classy.org>
2026-01-24 17:41:10 -0800

    feat: add login button

---
+++ stores/auth.ts
@@ -0,0 +1,39 @@
+import { createAuthClient } from "better-auth/client";
+
+const authClient = createAuthClient();
+
+export const useAuthStore = defineStore("useAuthStore", () => {
+  const loading = ref(false);
+
+  async function signIn() {
+    loading.value = true;
+    await authClient.signIn.social({
+      provider: "github",
+      callbackURL: "/dashboard",
+      errorCallbackURL: "/error",
+    });
+    loading.value = false;
+  }
+
+  return {
+    loading,
+    signIn,
+  };
+});
+
+// * OLD SYNTAX
+// export const useAuthStore = defineStore("useAuthStore", {
+//   state: () => ({
+//     loading: false,
+//   }),
+//   actions: {
+//     async signIn() {
+//       this.loading = true;
+//       await authClient.signIn.social({
+//         provider: "github",
+//         callbackURL: "/dashboard",
+//       });
+//       this.loading = false;
+//     },
+//   },
+// });
